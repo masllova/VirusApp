f699f70f41a204742d072b0dc4a134914fed456c
alexa <allexa.msv@gmail.com>
2023-05-04 17:42:57 +0300

    Initial Commit

---
+++ VirusApp/VirusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirusAppApp.swift
+//  VirusApp
+//
+//  Created by Александра Маслова on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct VirusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
